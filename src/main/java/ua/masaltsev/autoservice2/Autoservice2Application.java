85dfce7b3cfc7ecb955c9cac2bf4b6a5d75419f4
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2023-02-02 19:09:18 +0200

    initial commit

---
+++ src/main/java/ua/masaltsev/autoservice2/Autoservice2Application.java
@@ -0,0 +1,13 @@
+package ua.masaltsev.autoservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Autoservice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Autoservice2Application.class, args);
+    }
+
+}
