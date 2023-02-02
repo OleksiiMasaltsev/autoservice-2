be2690ca788d8e8f5308b3cef5d20bda809bf73f
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2023-02-02 23:46:31 +0200

    before controllers

--- src/main/java/ua/masaltsev/autoservice2/dto/mapper/CarMapper.java
+++ src/main/java/ua/masaltsev/autoservice2/dto/mapper/CarMapper.java
@@ -1,5 +1,24 @@
 package ua.masaltsev.autoservice2.dto.mapper;
 
-public interface CarMapper {
+import ua.masaltsev.autoservice2.dto.request.CarRequestDto;
+import ua.masaltsev.autoservice2.dto.response.CarResponseDto;
+import ua.masaltsev.autoservice2.model.Car;
 
+public class CarMapper implements RequestDtoMapper<CarRequestDto,
+        Car>, ResponseDtoMapper<CarResponseDto, Car> {
+    @Override
+    public Car mapToModel(CarRequestDto dto) {
+//        Car car = new Car();
+//        car.setModel(dto.getModel());
+//        car.setYear(dto.getYear());
+//        car.setBrand(dto.getBrand());
+//        car.setPlate(dto.getPlate());
+//        car.setOwner(dto.getOwnerId());
+        return null;
+    }
+
+    @Override
+    public CarResponseDto mapToDto(Car car) {
+        return null;
+    }
 }
