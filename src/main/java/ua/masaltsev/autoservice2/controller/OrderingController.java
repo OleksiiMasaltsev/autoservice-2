ac5d806dc540263983e39049b52d58ec8011de5d
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2023-02-03 22:56:08 +0200

    ordering controller before updating of status

--- src/main/java/ua/masaltsev/autoservice2/controller/OrderingController.java
+++ src/main/java/ua/masaltsev/autoservice2/controller/OrderingController.java
@@ -1,4 +1,59 @@
 package ua.masaltsev.autoservice2.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ua.masaltsev.autoservice2.dto.mapper.OrderingMapper;
+import ua.masaltsev.autoservice2.dto.mapper.ProductMapper;
+import ua.masaltsev.autoservice2.dto.request.OrderingRequestDto;
+import ua.masaltsev.autoservice2.dto.response.OrderingResponseDto;
+import ua.masaltsev.autoservice2.model.Ordering;
+import ua.masaltsev.autoservice2.model.Product;
+import ua.masaltsev.autoservice2.service.OrderingService;
+import ua.masaltsev.autoservice2.service.ProductService;
+
+@RestController
+@RequestMapping("/orderings")
 public class OrderingController {
+    private final OrderingService orderingService;
+    private final ProductService productService;
+    private final OrderingMapper orderingMapper;
+    private final ProductMapper productMapper;
+
+    public OrderingController(OrderingService orderingService,
+                              ProductService productService,
+                              OrderingMapper orderingMapper,
+                              ProductMapper productMapper) {
+        this.orderingService = orderingService;
+        this.productService = productService;
+        this.orderingMapper = orderingMapper;
+        this.productMapper = productMapper;
+    }
+
+    @PostMapping
+    public OrderingResponseDto save(@RequestBody OrderingRequestDto requestDto) {
+        return orderingMapper.mapToDto(
+                orderingService.save(orderingMapper.mapToModel(requestDto)));
+    }
+
+    @PostMapping("/product")
+    public OrderingResponseDto addProduct(@RequestParam Long productId,
+                                          @RequestParam Long orderingId) {
+        Product product = productService.getById(productId);
+        Ordering ordering = orderingService.getById(orderingId);
+        ordering.getProducts().add(product);
+        return orderingMapper.mapToDto(orderingService.save(ordering));
+    }
+
+    @PutMapping("/{id}")
+    public OrderingResponseDto update(@RequestBody OrderingRequestDto requestDto,
+                                      @PathVariable Long id) {
+        Ordering ordering = orderingMapper.mapToModel(requestDto);
+        ordering.setId(id);
+        return orderingMapper.mapToDto(orderingService.save(ordering));
+    }
 }
