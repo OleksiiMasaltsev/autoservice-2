a009015d13455ff4fda0f794f601c27802a3f29c
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2023-02-03 14:54:35 +0200

    car controller

--- src/main/java/ua/masaltsev/autoservice2/controller/CarController.java
+++ src/main/java/ua/masaltsev/autoservice2/controller/CarController.java
@@ -1,18 +1,37 @@
 package ua.masaltsev.autoservice2.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ua.masaltsev.autoservice2.dto.mapper.CarMapper;
 import ua.masaltsev.autoservice2.dto.request.CarRequestDto;
 import ua.masaltsev.autoservice2.dto.response.CarResponseDto;
+import ua.masaltsev.autoservice2.model.Car;
 import ua.masaltsev.autoservice2.service.CarService;
 
 @RestController
 @RequestMapping("/cars")
 public class CarController {
-    private CarService carService;
+    private final CarService carService;
+    private final CarMapper carMapper;
 
+    public CarController(CarService carService, CarMapper carMapper) {
+        this.carService = carService;
+        this.carMapper = carMapper;
+    }
+
+    @PostMapping
     public CarResponseDto save(@RequestBody CarRequestDto requestDto) {
-        return null;
+        return carMapper.mapToDto(carService.save(carMapper.mapToModel(requestDto)));
+    }
+
+    @PutMapping("/{id}")
+    public CarResponseDto update(@RequestBody CarRequestDto requestDto, @PathVariable Long id) {
+        Car car = carMapper.mapToModel(requestDto);
+        car.setId(id);
+        return carMapper.mapToDto(carService.save(car));
     }
 }
