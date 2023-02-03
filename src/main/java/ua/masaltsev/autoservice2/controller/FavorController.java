5e99e6fbac79f819fd9d40fa58d3a0889544bf14
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2023-02-03 15:01:08 +0200

    favor started

--- src/main/java/ua/masaltsev/autoservice2/controller/FavorController.java
+++ src/main/java/ua/masaltsev/autoservice2/controller/FavorController.java
@@ -1,4 +1,29 @@
 package ua.masaltsev.autoservice2.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.masaltsev.autoservice2.dto.mapper.FavorMapper;
+import ua.masaltsev.autoservice2.dto.request.FavorRequestDto;
+import ua.masaltsev.autoservice2.dto.response.FavorResponseDto;
+import ua.masaltsev.autoservice2.service.FavorService;
+
+@RestController
+@RequestMapping("/favors")
 public class FavorController {
+    private final FavorService favorService;
+    private final FavorMapper favorMapper;
+
+    public FavorController(FavorService favorService, FavorMapper favorMapper) {
+        this.favorService = favorService;
+        this.favorMapper = favorMapper;
+    }
+
+    @PostMapping
+    public FavorResponseDto save(@RequestBody FavorRequestDto requestDto) {
+        return favorMapper.mapToDto(favorService.save(favorMapper.mapToModel(requestDto)));
+    }
+
+
 }
